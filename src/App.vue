38c1780f00208089387d56bbe1c44534e2a8acd4
zeratini <gritterb@mail.gvsu.edu>
2024-03-13 18:18:26 +0000

    Added other options for types of drink

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Beverage :isIced="currentTemp === 'Cold'" />
     <ul>
       <li>
+        <span>Temperature:</span>
         <template v-for="temp in temps" :key="temp">
           <label>
             <input
@@ -16,6 +17,54 @@
           </label>
         </template>
       </li>
+
+      <li>
+        <span>Creamer:</span>
+        <template v-for="creamer in creamers" :key="creamer">
+          <label>
+            <input
+              type="radio"
+              name="Creamer"
+              :id="`r${creamer}`"
+              :value="creamer"
+              v-model="currentCream"
+            />
+            {{ creamer }}
+          </label>
+        </template>
+      </li>
+
+      <li>
+        <span>Syrup:</span>
+        <template v-for="syrup in syrups" :key="syrup">
+          <label>
+            <input
+              type="radio"
+              name="Syrup"
+              :id="`r${syrup}`"
+              :value="syrup"
+              v-model="currentSyrup"
+            />
+            {{ syrup }}
+          </label>
+        </template>
+      </li>
+
+      <li>
+        <span>Beverage:</span>
+        <template v-for="beverage in beverages" :key="beverage">
+          <label>
+            <input
+              type="radio"
+              name="Beverage"
+              :id="`r${beverage}`"
+              :value="beverage"
+              v-model="currentBeverage"
+            />
+            {{ beverage }}
+          </label>
+        </template>
+      </li>
     </ul>
   </div>
 </template>
@@ -25,7 +74,14 @@ import { ref } from "vue";
 import Beverage from "./components/Beverage.vue";
 // Define reactive data
 const temps = ref(["Hot", "Cold"]);
+const creamers = ref(["None", "Milk", "Cream", "Half & Half"]);
+const syrups = ref(["None", "Vanilla", "Caramel", "Hazelnut"]);
+const beverages = ref(["Coffee", "Green Tea", "Black Tea"]);
+
 const currentTemp = ref("Hot");
+const currentCream = ref("None");
+const currentSyrup = ref("None");
+const currentBeverage = ref("Coffee");
 </script>
 
 <style lang="scss">
